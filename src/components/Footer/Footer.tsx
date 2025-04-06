de0b5c3845dfeb2278706958bff30bb6b3b0089b
SmauRobert <robertgeorgesmau@gmail.com>
2025-04-06 23:27:47 +0300

    collapsable footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,32 +1,56 @@
+import { useState } from "react";
 import "./Footer.scss";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa"
 
 const Footer: React.FC = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed);
+    }
+    
     return (
         <footer className="footer">
-            <div className="footer-section">
-                <div className="footer-section-table">
-                    <div><b>Email</b></div>
-                    <div>contact@sportstogo.com</div>
-                    <div><b>Phone</b></div>
-                    <div>0765 432 198</div>
+             <div className={`footer-section ${collapsed ? "collapsed" : ""}`}>
+                <div className="footer-left">
+                    <div className="footer-2-column">
+                        <div><b>Email</b></div>
+                        <div>contact@sportstogo.com</div>
+                        <div><b>Phone</b></div>
+                        <div>0765 432 198</div>
+                    </div>
                 </div>
 
-                <div className="footer-section-table">
-                    <div>Somewhere No. 42</div>
-                    <div><b>Street</b></div>
-                    <div>Iasi, Romania</div>
-                    <div><b>City</b></div>
+                <div className="footer-center">
+                    {!collapsed && <FaChevronDown onClick={toggleCollapsed}/>}
+                </div>
+
+                <div className="footer-right">
+                    <div className="footer-2-column">
+                        <div>Somewhere No. 42</div>
+                        <div><b>Street</b></div>
+                        <div>Iasi, Romania</div>
+                        <div><b>City</b></div>
+                    </div>
                 </div>
             </div>
             <div className="footer-section">
-                <div>
-                    Sports-To-Go
+                <div className="footer-left">
+                    <div>
+                        Sports-To-Go
+                    </div>
                 </div>
 
-                <div className="footer-section-row">
-                    <a href="https://www.instagram.com/officialrickastley/">Instagram</a>
-                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">YouTube</a>
-                    <a href="https://www.tiktok.com/@rickastleyofficial?lang=en">TikTok</a>
+                <div className="footer-center">
+                    {collapsed && <FaChevronUp onClick={toggleCollapsed}/>}
+                </div>
+
+                <div className="footer-right">
+                    <div className="footer-section-links">
+                        <a href="https://www.instagram.com/officialrickastley/">Instagram</a>
+                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">YouTube</a>
+                        <a href="https://www.tiktok.com/@rickastleyofficial?lang=en">TikTok</a>
+                    </div>
                 </div>
             </div>
         </footer>
