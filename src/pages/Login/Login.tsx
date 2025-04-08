897e5b7f882da9ba2f7148c6ab79c41f16d49ca6
TeodorBujorean <teodorbujorean@gmail.com>
2025-04-08 16:56:24 +0300

    Implement login firebase + register page+path

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,34 @@
-const Login = () => {
-  return (
-    <div>
-      login bro
-    </div>
-  )
-}
+import Layout from '../../components/Layout/Layout'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useState } from "react";
 
-export default Login
+//pentru testare la token
+//email: test@test.com
+//password: 123456
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const token = await userCredential.user.getIdToken();
+      console.log("Token:", token); // trimite-l la backend
+    } catch (err) {
+      console.error(err,"Error logging in");
+    }
+  };
+
+  return (
+    <Layout>
+    <div>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+    </Layout>
+  );
+};
+
+export default Login;
---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,13 @@
+import Layout from '../../components/Layout/Layout'
+
+const Register = () => {
+  return (
+    <Layout>
+        <div>
+            Register bruv
+        </div>
+    </Layout>
+  )
+}
+
+export default Register
