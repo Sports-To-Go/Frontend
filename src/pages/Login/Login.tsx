5ee66eab8389f0f123d60f9974c12e1228627a92
SerbanStefan123 <serbanstefan208@gmail.com>
2025-04-17 11:41:10 +0300

    Created Register

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -8,6 +8,10 @@ const Login = () => {
 	const [username, setUsername] = useState('')
 	const [password, setPassword] = useState('')
 	const [showPassword, setShowPassword] = useState(false)
+	const [rotated, setRotated] = useState(false)
+	const [isRegister, setIsRegister] = useState(false)
+	const [registerUsername, setRegisterUsername] = useState('')
+	const [rePassword, setRePassword] = useState('')
 
 	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault()
@@ -19,13 +23,47 @@ const Login = () => {
 		setShowPassword(prev => !prev)
 	}
 
+	const handleRotateToRegister = () => {
+		setRotated(true)
+		setTimeout(() => {
+			setIsRegister(true)
+			setShowPassword(true)
+			setUsername('')
+			setPassword('')
+			setRePassword('')
+			setRegisterUsername('')
+		}, 300)
+	}
+
+	const handleRotateToLogin = () => {
+		setRotated(false)
+		setTimeout(() => {
+			setIsRegister(false)
+			setShowPassword(false)
+			setUsername('')
+			setPassword('')
+			setRePassword('')
+			setRegisterUsername('')
+		}, 300)
+	}
+
 	return (
 		<Layout showFooter={true} showTabs={false}>
 			<div className="login-page">
-				<div className="login-card">
+			<div className={`login-card ${rotated ? 'rotate' : ''} ${isRegister ? 'register-mode' : 'login-mode'}`}>
 					<CiLocationOn size={60} color="#05c69d" style={{ marginBottom: '-30px' }} />
-					<h1>Log in</h1>
+					<h1>{isRegister ? 'Register' : 'Log in'}</h1>
 					<form onSubmit={handleSubmit}>
+						{isRegister && (
+							<>
+								<input
+									type="text"
+									placeholder="Username"
+									value={registerUsername}
+									onChange={e => setRegisterUsername(e.target.value)}
+								/>
+							</>
+						)}
 						<input
 							type="email"
 							placeholder="Email address"
@@ -39,19 +77,45 @@ const Login = () => {
 								value={password}
 								onChange={e => setPassword(e.target.value)}
 							/>
-							<span onClick={togglePassword} className="eye-icon">
-								{showPassword ? <RiEyeFill /> : <RiEyeCloseFill />}
-							</span>
+							{isRegister && (
+								<input
+									className="repeat-password-input"
+									type={showPassword ? 'text' : 'password'}
+									placeholder="Repeat Password"
+									value={rePassword}
+									onChange={e => setRePassword(e.target.value)}
+								/>
+							)}
+							{!isRegister && (
+								<span onClick={togglePassword} className="eye-icon">
+									{showPassword ? <RiEyeFill /> : <RiEyeCloseFill />}
+								</span>
+							)}
 						</div>
-						<div className="options">
-							<a href="#">Forgot password?</a>
-						</div>
-						<button type="submit">Log in</button>
+						{!isRegister && (
+							<div className="options">
+								<a href="#">Forgot password?</a>
+							</div>
+						)}
+						<button type="submit">{isRegister ? 'Register' : 'Log in'}</button>
 						<div className="signup">
-							Don't have an account? <a href="#">Sign up</a>
+							{isRegister ? (
+								<>
+									Already have an account?{' '}
+									<a href="#" onClick={handleRotateToLogin}>
+										Sign in
+									</a>
+								</>
+							) : (
+								<>
+									Don't have an account?{' '}
+									<a href="#" onClick={handleRotateToRegister}>
+										Sign up
+									</a>
+								</>
+							)}
 						</div>
 						<div className="signup">
-							{' '}
 							<a href="#">Or Sign Up Using </a>
 						</div>
 						<div className="social-icons">
