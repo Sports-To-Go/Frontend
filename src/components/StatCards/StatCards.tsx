dbaf8888dda5266a6fd66ca743ecbadb77e88618
DraganClaudia <arandojust@gmail.com>
2025-04-19 12:37:36 +0300

    Update StatCards

--- src/components/StatCards/StatCards.tsx
+++ src/components/StatCards/StatCards.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './StatCards.scss';
+
 interface StatCardProps {
   title: string;
   subtitle: string;
@@ -16,27 +17,4 @@ const StatCard: React.FC<StatCardProps> = ({ title, subtitle, value }) => (
   </div>
 );
 
-const StatsCards: React.FC = () => {
-  const cards = [
-    { id: 'bookings', title: 'Total', subtitle: 'Bookings', value: '367' },
-    { id: 'venues', title: 'Active', subtitle: 'Venues', value: '10' },
-    { id: 'new-users', title: 'New', subtitle: 'Users', value: '14' },
-    { id: 'total-users', title: 'Total', subtitle: 'Users', value: '1.340' },
-    { id: 'revenue', title: 'Revenue', subtitle: 'This Month', value: '$2.786' }
-  ];
-
-  return (
-    <div className="stats-grid">
-      {cards.map((card) => (
-        <StatCard
-          key={card.id}
-          title={card.title}
-          subtitle={card.subtitle}
-          value={card.value}
-        />
-      ))}
-    </div>
-  );
-};
-
-export default StatsCards;
+export default StatCard;
--- src/pages/Administration/Administration.tsx
+++ src/pages/Administration/Administration.tsx
@@ -3,18 +3,35 @@ import Layout from '../../components/Layout/Layout'
 import './Administration.scss'
 import AdminTable from '../../components/AdminTable/AdminTable'
 import { tableData } from '../../assets/dummy-data'
-import StatCards from '../../components/StatCards/StatCards'
+import '../../components/StatCards/StatCards.scss'
+import StatCard from '../../components/StatCards/StatCards'
 
 export const Administration: React.FC = () => {
 	const headerTable = ['Venue', 'Name', 'Type', 'Status', 'Bookings', 'Ratings']
 	const [manageTitle, setManageTitle] = useState('Venues')
+	const cards = [
+		{ id: 'bookings', title: 'Total', subtitle: 'Bookings', value: '367' },
+		{ id: 'venues', title: 'Active', subtitle: 'Venues', value: '10' },
+		{ id: 'new-users', title: 'New', subtitle: 'Users', value: '14' },
+		{ id: 'total-users', title: 'Total', subtitle: 'Users', value: '1.340' },
+		{ id: 'revenue', title: 'Revenue', subtitle: 'This Month', value: '$2.786' }
+	  ];
 
 	return (
 		<Layout showTabs={false} showFooter={true}>
 			<div className="administration--container">
 				<h2>Admin dashboard</h2>
 				<div className="admin-cards--container">
-					<StatCards />
+				<div className="stats-grid">
+            		{cards.map((card) => (
+              			<StatCard
+			                key={card.id}
+            	    		title={card.title}
+                			subtitle={card.subtitle}
+                			value={card.value}
+		              />
+        		    ))}
+          			</div>
 				</div>
 				<div className="manage--container">
 					<div className="admin-tabs--container">To be implemented</div>
