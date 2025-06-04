0338f6da31dfa6742f7577a7129b226b37c36c86
DraganClaudia <arandojust@gmail.com>
2025-06-04 20:02:29 +0300

    I am trying to make it work.

--- src/components/ActivityFeed/ActivityFeed.tsx
+++ src/components/ActivityFeed/ActivityFeed.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './ActivityFeed.scss';
 import ActivityItem from '../ActivityItem/ActivityItem';
 import { useAuth } from '../../context/UserContext';
